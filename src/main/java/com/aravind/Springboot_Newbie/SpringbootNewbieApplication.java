8e442d914d7efc9b7eb7066d48f5110eeb4d9b32
Chinthapally Aravind <aravindchinthapally@gmail.com>
2026-06-15 23:03:13 +0530

    Integrated HTML and CSS with Springboot

---
+++ src/main/java/com/aravind/Springboot_Newbie/SpringbootNewbieApplication.java
@@ -0,0 +1,13 @@
+package com.aravind.Springboot_Newbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNewbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNewbieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aravind/Springboot_Newbie/SpringbootNewbieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravind.Springboot_Newbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNewbieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
